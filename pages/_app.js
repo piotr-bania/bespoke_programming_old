cb688482dadfedf1655e2fdfdc16bf4e128273d6
Piotr <piotr.bania@europe.com>
2022-06-28 16:27:15 +0100

    init

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,19 @@
 import '../styles/globals.css'
 import Layout from '../components/Layout'
+import { Canvas, useThree } from '@react-three/fiber'
+import MultipleSpheres from '../public/model1/MultipleSpheres'
 
 function MyApp({ Component, pageProps }) {
 return (
 <Layout>
   <Component {...pageProps} />
+  <Canvas style={{ position: "absolute", top: "0", left: "0", zIndex: "-1", width: "100vw", height: "100vh" }}>
+    <MultipleSpheres />
+    <ambientLight />
+    <pointLight position={[-5, -5, 0]} />
+  </Canvas>
 </Layout>
+
 )
 }
 
---
+++ public/model1/MultipleSpheres.js
@@ -0,0 +1,14 @@
+import React, { useRef } from 'react'
+import { useGLTF } from '@react-three/drei'
+
+export default function MultipleSpheres({ ...props }) {
+  const group = useRef()
+  const { nodes, materials } = useGLTF('/model1/multipleSpheres.gltf')
+  return (
+    <group ref={group} {...props} dispose={null} scale={4}>
+      <mesh geometry={nodes.Plane.geometry} material={nodes.Plane.material} />
+    </group>
+  )
+}
+
+useGLTF.preload('/model1/multipleSpheres.gltf')
