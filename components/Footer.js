94add803ff1167a3149d6cc083391052d7bca355
Piotr <piotr.bania@europe.com>
2022-07-06 11:30:11 +0100

    init

--- components/Footer.js
+++ components/Footer.js
@@ -2,24 +2,11 @@ import React from 'react'
 
 const Footer = () => {
 return (
-<div>
-    <section className="footer">
-        <div className="form">
-            <form action="https://formsubmit.co/contact@piotrbania.dev" method='POST'>
+    <section id="footer">
 
-                <h4>Get a Quote</h4>
-                
-                <input type="text" id='fname' name="firstname" placeholder="name *" required />
-                
-                <input type="email" id="email" name="email" placeholder="email address *" required></input>
+This is footer.
 
-                <textarea name="subject" id="subject" placeholder="message *" cols="30" rows="10"></textarea>
-
-                <button type='submit' value="Send" >Submit</button>
-            </form>
-        </div>
     </section>
-</div>
 )
 }
 
--- pages/quote.js
+++ pages/quote.js
@@ -1,11 +1,23 @@
 import React from 'react'
+import QuoteForm from '../components/QuoteForm'
 
 const quote = () => {
-  return (
-    <div>
-        <h1>Free quote.</h1>
-    </div>
-  )
+return (
+<>
+  
+<section id="quoteForm">
+<div className="gradientPurple"></div>
+  <h3 className="heading">Get a <abbr className='purple'>Free <br /></abbr>Quote.</h3>
+  <p className='description'>Please feel free to add as much information as possible. The more you tell me, the more
+    accurate I can be with
+    your
+    quote.</p>
+
+  <QuoteForm />
+</section>
+
+</>
+)
 }
 
 export default quote
