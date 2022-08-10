339d533312144bb0bf7f4d6a1762450988629aa4
Piotr <piotr.bania@europe.com>
2022-08-10 10:52:18 +0100

    init

--- components/canvas/CanvasHero.js
+++ components/canvas/CanvasHero.js
@@ -14,14 +14,10 @@ return (
             width: "150vw",
             height: "150vh"
             }}>
-        <ambientLight color="#8A3FFC" intensity={0.015} />
-        <pointLight color="#D0D0D1" intensity={0.15} position={[-2, 1, 2]} />
-        <directionalLight color="#697077" intensity={0.15} position={[-5, 0, 0]} />
-        {/* <Flex justifyContent="center" alignItems="center">
-            <Box centerAnchor flexGrow={1}>
-                <SphereWireframed />
-            </Box>
-        </Flex> */}
+        <ambientLight color="#FFFFFF" intensity={0.05} />
+        <pointLight color="#D0D0D1" intensity={0.25} position={[-2, 0, 2]} />
+        <directionalLight color="#697077" intensity={0.25} position={[-2, 0, 2]} />
+
         <NoiseAdvection />
         <OrbitControls autoRotate autoRotateSpeed={-0.25} />
     </Canvas>
